d344857ce0aa0db9e79f6c5c7b88732510b2d655
Daniil Ryabinin <70207219+dryabinin-actionengine@users.noreply.github.com>
2022-02-14 15:22:01 +0300

    feat(CI): add CI for project (#6)

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  launch: {
+    headless: process.env.CI === "true",
+    ignoreDefaultArgs: ["--disable-extensions"],
+    args: ["--no-sandbox"],
+  },
+  server: {
+    command: "yarn serve build",
+    port: 3000,
+    launchTimeout: 180000,
+  },
+};
