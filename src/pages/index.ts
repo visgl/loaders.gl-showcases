85985b76472e5c8aac0d46617d64fb6251ac7dab
Daniil Ryabinin <daniil.ryabinin@actionengine.com>
2022-02-17 15:34:06 +0300

    fix(dashboard): fix typo issues

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,3 @@
-export { Dashboard } from "./dasboard/dashboard";
+export { Dashboard } from "./dashboard/dashboard";
 export { ViewerApp } from "./viewer-app/viewer-app";
 export { DebugApp } from "./debug-app/debug-app";
