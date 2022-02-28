64a2a527c4ba8698df2090b79066acb7f35951ed
Daniil Ryabinin <70207219+dryabinin-actionengine@users.noreply.github.com>
2022-02-28 15:55:00 +0300

    test(unit): stats

--- src/utils/stats.ts
+++ src/utils/stats.ts
@@ -1,11 +1,12 @@
+import type { Tileset3D } from "@loaders.gl/tiles";
 import { Stats } from "@probe.gl/stats";
 
-export const initStats = (url = "") => {
+export const initStats = (url = ""): Stats => {
   const stats = new Stats({ id: url });
   return stats;
 };
 
-export const sumTilesetsStats = (tilesets, stats) => {
+export const sumTilesetsStats = (tilesets: Tileset3D[], stats: Stats): void => {
   stats.reset();
   for (const tileset of tilesets) {
     tileset.stats.forEach((stat) => {
