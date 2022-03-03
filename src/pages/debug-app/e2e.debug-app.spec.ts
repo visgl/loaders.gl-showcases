d5c8b67bb4e85cf9308474345b1b3ecaf279a7fe
Kirillov Zakhar <zakhar.kirillov@actionengine.com>
2022-03-03 18:25:21 +0300

    test(debug): additional e2e tests

--- src/pages/debug-app/e2e.debug-app.spec.ts
+++ src/pages/debug-app/e2e.debug-app.spec.ts
@@ -1,6 +1,6 @@
 import puppeteer from "puppeteer";
 
-describe("ViewerApp", () => {
+describe("DebugApp", () => {
   let browser;
   let page;
 
@@ -23,15 +23,15 @@ describe("ViewerApp", () => {
         "New York",
         "San Francisco v1.6",
         "San Francisco v1.7",
-        'Building'
+        "Building"
       ];
 
     await page.waitForSelector("#control-panel");
     await page.select("#base-map", "Light");
 
     for await (const item of controlOptions) {
-     const tileset = await page.select('#tilesets', item);    
-       expect(tileset).toEqual([item])
+      const tileset = await page.select("#tilesets", item);    
+        expect(tileset).toEqual([item])
         if (tileset.includes("Building")) {
         await page.waitForSelector("#building-explorer");
         await page.click("#BuildingExplorerToggle~span");
@@ -44,9 +44,9 @@ describe("ViewerApp", () => {
     await page.click("#BuildingExplorerToggle~span");
     await page.click("#terrain-layer-switch~span");
     await page.click("#select-map");
-  });
+  }, 10000);
 
-  it('Map Info tab works', async () => {
+  it("Map Info tab works", async () => {
     await page.click("#map-info");
     await page.waitForSelector("#map-info-panel", {visible: true});
   });
@@ -54,12 +54,12 @@ describe("ViewerApp", () => {
   it("Memory Usage tab works", async () => {
     await page.click("#memory-usage");
     await page.waitForSelector("#stats-widget", {visible: true});
-  });
+  }, 10000);
 
   it("Validator tab works", async () => {
     await page.click("#validator");
     await page.waitForSelector("#semantic-validator");
-  });
+  }, 10000);
 
   it("Debug panel works", async () => {
     await page.click("#debug-panel");
@@ -69,6 +69,36 @@ describe("ViewerApp", () => {
     await page.click("#pickable-icon");
     await page.click("#uvDebugTexture-icon");
     await page.click("#wireframe-icon");
+    const tileColorOptions = await page.$$eval("#color > option", options =>
+      options.map(option => option.textContent)
+    );
+    expect(tileColorOptions).toEqual([ "Original", "Random by tile", "By depth", "User selected"])
+    for await (const [index] of tileColorOptions.entries()) {
+      await page.select("#color", String(index));   
+    }
+
     await page.click("#boundingVolume~span");
-  });
+
+    const volumeTypeOptionsText = await page.$$eval("#volume-type > option", options =>
+      options.map(option => option.textContent)
+    );
+    const volumeTypeOptionsValue = await page.$$eval("#volume-type > option", options =>
+      options.map(option => option.value)
+    );
+    expect(volumeTypeOptionsText).toEqual(["MBS", "OBB"])
+    for await (const item of volumeTypeOptionsValue) {
+      await page.select('#volume-type', item);   
+    }
+
+    const volumeColorOptionsText = await page.$$eval("#volume-color > option", options =>
+      options.map(option => option.textContent)
+    );
+    const volumeColorOptionsValue = await page.$$eval("#volume-color > option", options =>
+      options.map(option => option.value)
+    );
+    expect(volumeColorOptionsText).toEqual(["White", "By tile"])
+    for await (const item of volumeColorOptionsValue) {
+      await page.select("#volume-color", item);   
+    }
+  }, 10000);
 });
