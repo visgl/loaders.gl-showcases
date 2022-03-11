ef412eda87a2f7d4acc9bfb4b920df21a61a754f
Kirillov Zakhar <zakhar.kirillov@actionengine.com>
2022-03-11 17:32:30 +0300

    test(attributes-panel): add unit tests

--- src/components/attributes-panel/attributes-panel.spec.tsx
+++ src/components/attributes-panel/attributes-panel.spec.tsx
@@ -1,18 +1,27 @@
-import { cleanup, render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { AttributesPanel } from "./attributes-panel";
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
 
-afterEach(cleanup);
 
 describe("Attributes Panel", () => {
-  test("Should render attributes panel", () => {
+  it("Should render attributes panel", () => {
+    const handleClosePanel = jest.fn();
     render(
-      <AttributesPanel
-        title={"Attributes panel"}
-        attributesObject={{}}
-        handleClosePanel={() => {
-          console.log("handleClosePanel");
-        }}
-      />
-    );
+        <AttributesPanel
+          title={"Attributes panel"}
+          attributesObject={{}}
+          handleClosePanel={handleClosePanel}
+        />
+      );
+      const button = screen.getByRole("button");
+      const heading = screen.getByRole("heading");
+      expect(heading).toBeInTheDocument();
+      expect(button).toBeInTheDocument();
+      expect(screen.getByRole("table")).toBeInTheDocument();
+      expect(screen.getByRole("rowgroup")).toBeInTheDocument();
+      expect(heading).toHaveTextContent(/Attributes panel/i);
+      userEvent.click(button);
+      expect(handleClosePanel).toHaveBeenCalledTimes(1);
+    });
   });
-});
