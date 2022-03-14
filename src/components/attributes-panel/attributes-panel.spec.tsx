f812fe668887c9f432000dc3ba0f89215fd80620
Daniil Ryabinin <daniil.ryabinin@actionengine.com>
2022-03-14 18:36:18 +0300

    test(unit): move import to jest.setup file

--- src/components/attributes-panel/attributes-panel.spec.tsx
+++ src/components/attributes-panel/attributes-panel.spec.tsx
@@ -1,27 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import { AttributesPanel } from "./attributes-panel";
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom'
-
+import userEvent from "@testing-library/user-event";
 
 describe("Attributes Panel", () => {
   it("Should render attributes panel", () => {
     const handleClosePanel = jest.fn();
     render(
-        <AttributesPanel
-          title={"Attributes panel"}
-          attributesObject={{}}
-          handleClosePanel={handleClosePanel}
-        />
-      );
-      const button = screen.getByRole("button");
-      const heading = screen.getByRole("heading");
-      expect(heading).toBeInTheDocument();
-      expect(button).toBeInTheDocument();
-      expect(screen.getByRole("table")).toBeInTheDocument();
-      expect(screen.getByRole("rowgroup")).toBeInTheDocument();
-      expect(heading).toHaveTextContent(/Attributes panel/i);
-      userEvent.click(button);
-      expect(handleClosePanel).toHaveBeenCalledTimes(1);
-    });
+      <AttributesPanel
+        title={"Attributes panel"}
+        attributesObject={{}}
+        handleClosePanel={handleClosePanel}
+      />
+    );
+    const button = screen.getByRole("button");
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(screen.getByRole("table")).toBeInTheDocument();
+    expect(screen.getByRole("rowgroup")).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/Attributes panel/i);
+    userEvent.click(button);
+    expect(handleClosePanel).toHaveBeenCalledTimes(1);
   });
+});
