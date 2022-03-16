de9a23aaeb458399192a457f35776d00602976eb
Kirillov Zakhar <97729748+kirillovzah@users.noreply.github.com>
2022-03-16 14:33:26 +0300

    test(attributes-panel): add unit tests (#53)

--- src/components/attributes-panel/attributes-panel.spec.tsx
+++ src/components/attributes-panel/attributes-panel.spec.tsx
@@ -1,18 +1,35 @@
-import { cleanup, render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { AttributesPanel } from "./attributes-panel";
-
-afterEach(cleanup);
+import userEvent from "@testing-library/user-event";
 
 describe("Attributes Panel", () => {
-  test("Should render attributes panel", () => {
+  it("Should render attributes panel", () => {
+    const handleClosePanel = jest.fn();
     render(
       <AttributesPanel
         title={"Attributes panel"}
-        attributesObject={{}}
-        handleClosePanel={() => {
-          console.log("handleClosePanel");
+        attributesObject={{
+          NAME: "Sanfran_Orig_0992.flt",
+          OBJECTID: "74689"
         }}
+        handleClosePanel={handleClosePanel}
       />
     );
+    const button = screen.getByRole("button");
+    const heading = screen.getByRole("heading");
+    const getCells = (cellName) => screen.getByRole("cell", {name: cellName});
+    const getColumnheader = (columnheaderName) => screen.getByRole('columnheader', {name: columnheaderName});
+    expect(heading).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(screen.getByRole("table")).toBeInTheDocument();
+    expect(screen.getByRole("rowgroup")).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/Attributes panel/i);
+    expect(screen.getByText("Sanfran_Orig_0992.flt")).toBeInTheDocument();
+    expect(getCells("74689")).toBeInTheDocument();
+    expect(getCells("Sanfran_Orig_0992.flt")).toBeInTheDocument();
+    expect(getColumnheader("NAME")).toBeInTheDocument();
+    expect(getColumnheader("OBJECTID")).toBeInTheDocument();
+    userEvent.click(button);
+    expect(handleClosePanel).toHaveBeenCalledTimes(1);
   });
 });
