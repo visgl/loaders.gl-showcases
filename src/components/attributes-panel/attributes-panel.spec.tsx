7d65e9fd7cd907f061bd3c92d07255dc3a8dc379
Kirillov Zakhar <zakhar.kirillov@actionengine.com>
2022-03-17 01:46:55 +0300

    test: test attributes panel without data

--- src/components/attributes-panel/attributes-panel.spec.tsx
+++ src/components/attributes-panel/attributes-panel.spec.tsx
@@ -3,11 +3,11 @@ import { AttributesPanel } from "./attributes-panel";
 import userEvent from "@testing-library/user-event";
 
 describe("Attributes Panel", () => {
-  it("Should render attributes panel", () => {
+  it("Should render attributes panel with data", () => {
     const handleClosePanel = jest.fn();
     render(
       <AttributesPanel
-        title={"Attributes panel"}
+        title={"Sanfran_Orig_0992.flt"}
         attributesObject={{
           NAME: "Sanfran_Orig_0992.flt",
           OBJECTID: "74689"
@@ -18,18 +18,38 @@ describe("Attributes Panel", () => {
     const button = screen.getByRole("button");
     const heading = screen.getByRole("heading");
     const getCells = (cellName) => screen.getByRole("cell", {name: cellName});
-    const getColumnheader = (columnheaderName) => screen.getByRole('columnheader', {name: columnheaderName});
+    const getColumnheader = (columnheaderName) => screen.getByRole("columnheader", {name: columnheaderName});
     expect(heading).toBeInTheDocument();
     expect(button).toBeInTheDocument();
     expect(screen.getByRole("table")).toBeInTheDocument();
     expect(screen.getByRole("rowgroup")).toBeInTheDocument();
-    expect(heading).toHaveTextContent(/Attributes panel/i);
-    expect(screen.getByText("Sanfran_Orig_0992.flt")).toBeInTheDocument();
+    expect(heading).toHaveTextContent("Sanfran_Orig_0992.flt");
     expect(getCells("74689")).toBeInTheDocument();
     expect(getCells("Sanfran_Orig_0992.flt")).toBeInTheDocument();
     expect(getColumnheader("NAME")).toBeInTheDocument();
     expect(getColumnheader("OBJECTID")).toBeInTheDocument();
     userEvent.click(button);
     expect(handleClosePanel).toHaveBeenCalledTimes(1);
+  });
+
+  it("Should render attributes panel without data", () => {
+    const handleClosePanel = jest.fn();
+    render(
+      <AttributesPanel
+        title={""}
+        attributesObject={{
+          NAME: "",
+          OBJECTID: ""
+        }}
+        handleClosePanel={handleClosePanel}
+      />
+    );
+    const heading = screen.queryByRole("heading");
+    const cells = screen.getAllByRole("cell").map(cell => cell.textContent);
+    const getColumnheader = (columnheaderName) => screen.getByRole("columnheader", {name: columnheaderName});
+    expect(heading).not.toBeInTheDocument();
+    expect(cells).toEqual(["No Data", "No Data"])
+    expect(getColumnheader("NAME")).toBeInTheDocument();
+    expect(getColumnheader("OBJECTID")).toBeInTheDocument();
   });
 });
