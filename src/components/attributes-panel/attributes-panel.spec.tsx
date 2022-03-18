6cb315ba9236f7c779b9332c83a08b4a3e783c02
Kirillov Zakhar <zakhar.kirillov@actionengine.com>
2022-03-18 16:38:27 +0300

    chore: remove useless import

--- src/components/attributes-panel/attributes-panel.spec.tsx
+++ src/components/attributes-panel/attributes-panel.spec.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import { AttributesPanel } from "./attributes-panel";
-import {TileValidator} from "../tile-validator/tile-validator";
 import userEvent from "@testing-library/user-event";
 
 describe("Attributes Panel", () => {
