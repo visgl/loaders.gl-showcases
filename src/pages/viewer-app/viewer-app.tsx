55f133ab39e9e4cabc2fe17c3453079e3a39c3ed
Victor Belomestnov <belom88@yandex.ru>
2022-06-16 14:01:50 +0300

    chore: make EXAMPLES in array representation

--- src/pages/viewer-app/viewer-app.tsx
+++ src/pages/viewer-app/viewer-app.tsx
@@ -21,10 +21,7 @@ import {
 } from "@loaders.gl/i3s";
 import { StatsWidget } from "@probe.gl/stats-widget";
 
-import {
-  ControlPanel,
-  BuildingExplorer,
-} from "../../components";
+import { ControlPanel, BuildingExplorer } from "../../components";
 import {
   parseTilesetFromUrl,
   parseTilesetUrlParams,
@@ -36,12 +33,13 @@ import {
 } from "../../utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { INITIAL_EXAMPLE_NAME, EXAMPLES } from "../../constants/i3s-examples";
+import { INITIAL_EXAMPLE, EXAMPLES } from "../../constants/i3s-examples";
 import { INITIAL_MAP_STYLE } from "../../constants/map-styles";
 import { CUSTOM_EXAMPLE_VALUE } from "../../constants/i3s-examples";
 import { Tile3D, Tileset3D } from "@loaders.gl/tiles";
 import { TileDetailsPanel } from "../../components/tile-details-panel/tile-details-panel";
 import { FeatureAttributes } from "../../components/feature-attributes/feature-attributes";
+import { LayerExample } from "../../utils/types";
 
 const TRANSITION_DURAITON = 4000;
 
@@ -145,7 +143,7 @@ export const ViewerApp = () => {
     useState<StatsWidget | null>(null);
   const [loadedTilesets, setLoadedTilesets] = useState<Tileset3D[]>([]);
 
-  const initMainTileset = () => {
+  const initMainTileset = (): LayerExample => {
     const tilesetParam = parseTilesetFromUrl();
 
     if (tilesetParam?.startsWith("http")) {
@@ -159,7 +157,7 @@ export const ViewerApp = () => {
       return EXAMPLES[tilesetParam];
     }
 
-    return EXAMPLES[INITIAL_EXAMPLE_NAME];
+    return INITIAL_EXAMPLE;
   };
 
   const [mainTileset, setMainTileset] = useState(initMainTileset());
