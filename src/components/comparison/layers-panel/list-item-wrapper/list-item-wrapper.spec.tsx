6f672e0593fe9ca62a4e2382e7d77c58699a3645
Kirillov Zakhar <zakhar.kirillov@actionengine.com>
2022-07-12 14:28:42 +0300

    fix comments

--- src/components/comparison/layers-panel/list-item-wrapper/list-item-wrapper.spec.tsx
+++ src/components/comparison/layers-panel/list-item-wrapper/list-item-wrapper.spec.tsx
@@ -1,16 +1,14 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-
 import userEvent from "@testing-library/user-event";
 import { ListItemWrapper } from "./list-item-wrapper";
 import { ExpandState } from "../../../../types";
 import { renderWithTheme } from "../../../../utils/testing-utils/render-with-theme";
 
 describe("List Item Wrapper", () => {
-  it("Should render list item wrapper", () => {
-    const onChange = jest.fn();
-    const onOptionsClick = jest.fn();
-    const onExpandClick = jest.fn();
+  const onChange = jest.fn();
+  const onOptionsClick = jest.fn();
+  const onExpandClick = jest.fn();
 
+  it("Should render list item wrapper", () => {
     renderWithTheme(
       <ListItemWrapper
         id="test-id"
@@ -24,30 +22,13 @@ describe("List Item Wrapper", () => {
         San Francisco v1.6
       </ListItemWrapper>
     );
-    const optionsIcon = document.querySelector("#test-id")!;
-    const expandIcon = document.querySelector("#test-id ~ div")!;
-    userEvent.click(expandIcon);
+    const optionsIcon = document.querySelector("#test-id");
+    const expandIcon = document.querySelector("#test-id ~ div");
+    expect(expandIcon).not.toBeNull();
+    expandIcon && userEvent.click(expandIcon);
     expect(onExpandClick).toHaveBeenCalledTimes(1);
-    userEvent.click(optionsIcon);
+    expect(optionsIcon).not.toBeNull();
+    optionsIcon && userEvent.click(optionsIcon);
     expect(onOptionsClick).toHaveBeenCalledTimes(1);
-  });
-  it("Should render expand with collapesed icon", () => {
-    const onChange = jest.fn();
-    const onOptionsClick = jest.fn();
-    const onExpandClick = jest.fn();
-
-    renderWithTheme(
-      <ListItemWrapper
-        id="test-id"
-        selected={true}
-        hasOptions={true}
-        expandState={ExpandState.collapsed}
-        onOptionsClick={onOptionsClick}
-        onExpandClick={onExpandClick}
-        onClick={onChange}
-      >
-        San Francisco v1.6
-      </ListItemWrapper>
-    );
   });
 });
