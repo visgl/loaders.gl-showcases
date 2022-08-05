d04704da631fe4f7aca9320a9b3329447e33f930
Daniil Ryabinin <daniil.ryabinin@actionengine.com>
2022-08-05 15:05:40 +0300

    feat(help-panel): remove not needed method

--- src/components/help-panel/help-panel.tsx
+++ src/components/help-panel/help-panel.tsx
@@ -34,10 +34,6 @@ export const HelpPanel = ({ onClose }: HelpPanelProps) => {
     setShortcuts(shortcutsList[selectedTab]);
   }, [selectedTab, theme]);
 
-  const onShortcutHover = (shortcutId) => {
-    setActiveShortcutId(shortcutId);
-  };
-
   if (isDesktop) {
     return (
       <DesktopHelpPanel
@@ -46,7 +42,7 @@ export const HelpPanel = ({ onClose }: HelpPanelProps) => {
         onClose={onClose}
         shortcuts={shortcuts}
         activeShortcutId={activeShortcutId}
-        onShortcutHover={onShortcutHover}
+        onShortcutHover={setActiveShortcutId}
       />
     );
   } else {
