b7840d4d5e98c08878a0123c79860e2ee4c38f77
Kirillov Zakhar <zakhar.kirillov@actionengine.com>
2022-11-28 03:53:47 +0300

    feat(compare-button): improve behavior

--- src/components/comparison/compare-button/compare-button.spec.tsx
+++ src/components/comparison/compare-button/compare-button.spec.tsx
@@ -1,7 +1,12 @@
 import { renderWithTheme } from "../../../utils/testing-utils/render-with-theme";
 import { CompareButton } from "./compare-button";
 import { CompareButtonMode } from "../../../types";
-import { fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { useAppLayout } from "../../../utils/hooks/layout";
+
+jest.mock("../../../utils/hooks/layout");
+
+const useAppLayoutMock = useAppLayout as unknown as jest.Mocked<any>;
 
 const onCompareModeToggle = jest.fn();
 const onDownloadClick = jest.fn();
@@ -22,6 +27,7 @@ const callRender = (renderFunc, props = {}) => {
 
 describe("CompareButton", () => {
   it("Should render start comparing", () => {
+    useAppLayoutMock.mockImplementation(() => "desktop");
     const { container, getByText } = callRender(renderWithTheme);
     const buttonContainer = getByText("Start comparing");
     expect(container).toBeInTheDocument();
@@ -31,6 +37,7 @@ describe("CompareButton", () => {
   });
 
   it("Should render comparing", () => {
+    useAppLayoutMock.mockImplementation(() => "desktop");
     const { getByText } = callRender(renderWithTheme, {
       compareButtonMode: CompareButtonMode.Comparing,
       disableButton: false,
@@ -41,6 +48,7 @@ describe("CompareButton", () => {
   });
 
   it("Should render download button", () => {
+    useAppLayoutMock.mockImplementation(() => "desktop");
     const { container } = callRender(renderWithTheme, {
       downloadStats: true,
     });
@@ -48,20 +56,49 @@ describe("CompareButton", () => {
   });
 
   it("Should click download button", () => {
+    useAppLayoutMock.mockImplementation(() => "desktop");
     const { container } = callRender(renderWithTheme, {
       downloadStats: true,
     });
     const downloadButton = container.firstChild.lastChild;
-    fireEvent.click(downloadButton);
+    userEvent.click(downloadButton);
     expect(onDownloadClick).toHaveBeenCalled();
   });
 
   it("Should disable download button when comparing is stopped", () => {
+    useAppLayoutMock.mockImplementation(() => "desktop");
     const { container } = callRender(renderWithTheme, {
       downloadStats: true,
       disableDownloadButton: true,
     });
     const downloadButton = container.firstChild.lastChild;
     expect(downloadButton).toBeDisabled();
+  });
+
+  it("Should disable button when tiles are not loaded", () => {
+    useAppLayoutMock.mockImplementation(() => "desktop");
+    const { container } = callRender(renderWithTheme);
+    const compareButton = container.firstChild.firstChild;
+    expect(compareButton).toBeDisabled();
+  });
+
+  it("Should show desktop tooltip when button is disabled", () => {
+    useAppLayoutMock.mockImplementation(() => "destop");
+    const { container, getByText } = callRender(renderWithTheme, {
+      disableButton: true,
+    });
+    const compareButton = container.firstChild;
+    userEvent.hover(compareButton);
+    getByText("You can start comparison when all tiles are fully loaded");
+    userEvent.unhover(compareButton);
+  });
+
+  it("Should show mobile tooltip when button is disabled", () => {
+    useAppLayoutMock.mockImplementation(() => "mobile");
+    const { container, getByText } = callRender(renderWithTheme);
+
+    const compareButton = container.firstChild.firstChild;
+    expect(compareButton).toBeDisabled();
+    getByText("You can start comparison when all tiles are fully loaded");
   });
 });
--- src/components/comparison/compare-button/compare-button.tsx
+++ src/components/comparison/compare-button/compare-button.tsx
@@ -1,16 +1,20 @@
-import styled from "styled-components";
-import { CompareButtonMode } from "../../../types";
+import styled, { css } from "styled-components";
+import { CompareButtonMode, Layout } from "../../../types";
 import { Title } from "../common";
 import StartIcon from "../../../../public/icons/start.svg";
 import StopIcon from "../../../../public/icons/stop.svg";
 import DownloadIcon from "../../../../public/icons/download.svg";
-import { getCurrentLayoutProperty, useAppLayout } from "../../../utils/hooks/layout";
+import {
+  getCurrentLayoutProperty,
+  useAppLayout,
+} from "../../../utils/hooks/layout";
+import { useState } from "react";
 
 type LayoutProps = {
   layout: string;
 };
 
-const Container = styled.div<LayoutProps>`
+const Container = styled.div<LayoutProps & { disableButton: boolean }>`
   position: absolute;
   ${getCurrentLayoutProperty({
     desktop: "top: 100px",
@@ -28,31 +32,74 @@ const Container = styled.div<LayoutProps>`
     tablet: "background: transparent",
     mobile: "background: transparent",
   })};
+  cursor: pointer;
   display: flex;
-  align-items: stretch;
   justify-content: center;
   border-radius: 12px;
   padding: 8px;
   gap: 8px;
   z-index: 1;
+  ${({ disableButton }) =>
+    disableButton &&
+    css`
+      cursor: not-allowed;
+    `}
 `;
 
-const Button = styled.button`
+const Button = styled.button<LayoutProps & { disableButton: boolean }>`
   display: flex;
   color: ${({ theme }) => theme.colors.mainHiglightColorInverted};
   padding: 12px;
   align-items: center;
   border-radius: 8px;
-  cursor: pointer;
   background-color: ${({ theme }) => theme.colors.mainColor};
   background-position: center;
   border: none;
   fill: ${({ theme }) => theme.colors.buttonIconColor};
+  cursor: inherit;
 
-  &:hover {
+  ${({ disableButton }) =>
+    disableButton &&
+    css`
+      ${getCurrentLayoutProperty({
+        desktop: "opacity: 0.4",
+        tablet: "opacity: 0.8",
+        mobile: "opacity: 0.8",
+      })};
+    `}
+
+  :not([disabled])&:hover {
     fill: ${({ theme }) => theme.colors.buttonDimIconColor};
     background-color: ${({ theme }) => theme.colors.buttonDimColor};
   }
+`;
+
+const Tooltip = styled.div<LayoutProps>`
+  position: absolute;
+  ${getCurrentLayoutProperty({
+    desktop: "top: 60px",
+    tablet: "top: 90%",
+    mobile: "top: 90%",
+  })};
+  left: ${getCurrentLayoutProperty({
+    desktop: "calc(50% - 200px)",
+    tablet: "calc(50% - 90px)",
+    mobile: "calc(50% - 90px)",
+  })};
+  background-color: ${({ theme }) => theme.colors.mainColor};
+  border-radius: 12px;
+  padding: 8px;
+  z-index: 1;
+  font-weight: 500;
+  font-style: normal;
+  font-size: 16px;
+  line-height: 19px;
+  color: ${({ theme }) => theme.colors.fontColor};
+  white-space: ${getCurrentLayoutProperty({
+    desktop: "nowrap",
+    tablet: "normal",
+    mobile: "normal",
+  })};
 `;
 
 const ButtonTitle = styled(Title)`
@@ -78,11 +125,35 @@ export const CompareButton = ({
   onCompareModeToggle,
   onDownloadClick,
 }: CompareButtonProps) => {
+  const [isHovering, setIsHovering] = useState<boolean>(false);
+
   const layout = useAppLayout();
 
+  const showTooltip =
+    (isHovering || layout !== Layout.Desktop) && disableButton;
+
+  const onPointerEnter = () => {
+    setIsHovering(true);
+  };
+
+  const onPointerLeave = () => {
+    setIsHovering(false);
+  };
+
   return (
-    <Container id="compare-button" layout={layout}>
-      <Button disabled={disableButton} onClick={onCompareModeToggle}>
+    <Container
+      id="compare-button"
+      layout={layout}
+      disableButton={disableButton}
+      onPointerEnter={onPointerEnter}
+      onPointerLeave={onPointerLeave}
+    >
+      <Button
+        layout={layout}
+        disabled={disableButton}
+        onClick={onCompareModeToggle}
+        disableButton={disableButton}
+      >
         {compareButtonMode === CompareButtonMode.Start && (
           <>
             <StartIcon />
@@ -97,9 +168,19 @@ export const CompareButton = ({
         )}
       </Button>
       {downloadStats && (
-        <Button disabled={disableDownloadButton} onClick={onDownloadClick}>
+        <Button
+          layout={layout}
+          disabled={disableDownloadButton}
+          disableButton={disableButton}
+          onClick={onDownloadClick}
+        >
           <DownloadIcon />
         </Button>
+      )}
+      {showTooltip && (
+        <Tooltip layout={layout}>
+          You can start comparison when all tiles are fully loaded
+        </Tooltip>
       )}
     </Container>
   );
