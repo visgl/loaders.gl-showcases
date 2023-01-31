5cb5cff43c4fa97b64470edaee99b7630f1f0975
Victor Belomestnov <belom88@yandex.ru>
2023-01-31 14:00:07 +0100

    fix(stats): Tile Memory Use type

--- src/utils/stats.spec.ts
+++ src/utils/stats.spec.ts
@@ -1,24 +1,54 @@
-import { initStats, sumTilesetsStats } from './stats';
+import { initStats, sumTilesetsStats } from "./stats";
 import { Stats } from "@probe.gl/stats";
 
-jest.mock('@probe.gl/stats', () => ({
-  Stats: jest.fn().mockImplementation(() => ({ reset: jest.fn(), get: jest.fn().mockImplementation(() => ({ addCount: jest.fn() })) }))
+const getStatMock = jest
+  .fn()
+  .mockImplementation(() => ({ addCount: jest.fn() }));
+jest.mock("@probe.gl/stats");
+// @ts-expect-error Property 'mockImplementation' does not exist on type 'typeof Stats'
+Stats.mockImplementation(() => ({
+  reset: jest.fn(),
+  get: getStatMock,
 }));
 
 describe("Stats", () => {
   test("Should init stats", () => {
-    const url = 'https://test.com';
+    const url = "https://test.com";
     const result = initStats(url);
     expect(result).toBeDefined();
   });
 
   test("Should sum tileset stats", () => {
-    const stats = new Stats({ id: 'test' });
-    const tilesets = [{
-      stats: [{ get: jest.fn().mockImplementation(() => ({ addCount: jest.fn() })) }]
-    }];
+    const stats = new Stats({ id: "test" });
+    const tilesets = [
+      {
+        stats: [
+          {
+            get: jest.fn().mockImplementation(() => ({ addCount: jest.fn() })),
+          },
+        ],
+      },
+    ];
 
+    // @ts-expect-error Type '{ stats: { get: Mock<any, any>; }[]; }' is missing the following properties from type 'Tileset3D' ...
     sumTilesetsStats(tilesets, stats);
   });
 
+  test("Should keep stat type", () => {
+    const stats = new Stats({ id: "test" });
+    const tilesets = [
+      {
+        stats: [
+          {
+            name: "Memory stat",
+            type: "memory",
+          },
+        ],
+      },
+    ];
+
+    // @ts-expect-error Type '{ stats: { get: Mock<any, any>; }[]; }' is missing the following properties from type 'Tileset3D' ...
+    sumTilesetsStats(tilesets, stats);
+    expect(getStatMock).toHaveBeenCalledWith("Memory stat", "memory");
+  });
 });
