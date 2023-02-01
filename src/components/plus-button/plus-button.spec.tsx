d4249e665056924ecdf1ccfcb72237e63e79fa8c
Daniil Ryabinin <daniil.ryabinin@actionengine.com>
2023-02-01 10:53:16 +0300

    chore(plus-button): add checks for button size

--- src/components/plus-button/plus-button.spec.tsx
+++ src/components/plus-button/plus-button.spec.tsx
@@ -19,16 +19,24 @@ describe("Plus Button", () => {
   it("Should render small Plus button", () => {
     const { container } = callRender(renderWithTheme, { children: 'Test Button' });
     expect(container).toBeInTheDocument();
-
-    userEvent.click(screen.getByText('Test Button'));
+    const button = screen.getByText('Test Button');
+    const buttonHeight = getComputedStyle(button.previousSibling as Element).getPropertyValue(
+      "height"
+    );
+    expect(buttonHeight).toEqual('24px');
+    userEvent.click(button);
     expect(onClickMock).toHaveBeenCalled();
   });
 
   it("Should render Big Plus button", () => {
     const { container } = callRender(renderWithTheme, { children: 'Test Button', buttonSize: ButtonSize.Big });
     expect(container).toBeInTheDocument();
-
-    userEvent.click(screen.getByText('Test Button'));
+    const button = screen.getByText('Test Button');
+    const buttonHeight = getComputedStyle(button.previousSibling as Element).getPropertyValue(
+      "height"
+    );
+    expect(buttonHeight).toEqual('40px');
+    userEvent.click(button);
     expect(onClickMock).toHaveBeenCalled();
   });
 });
