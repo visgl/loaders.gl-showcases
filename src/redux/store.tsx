41a4c61538c3e9d5c5f77ee8c3c605b110a70f0b
Maxim Kuznetsov <116376092+MaximKYZ@users.noreply.github.com>
2023-07-06 10:10:14 +0000

    chore: Move async debug texture loading to redux (#289)

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -6,18 +6,29 @@ import {
 import colorsByAttributeReducer from "./slices/colors-by-attribute-slice";
 import flattenedSublayersSliceReducer from "./slices/flattened-sublayers-slice";
 import dragModeSliceReducer from "./slices/drag-mode-slice";
+import uvDebugTextureSliceReducer from "./slices/uv-debug-texture-slice";
 
 // Create the root reducer separately so we can extract the RootState type
 const rootReducer = combineReducers({
   colorsByAttribute: colorsByAttributeReducer,
   flattenedSublayers: flattenedSublayersSliceReducer,
   dragMode: dragModeSliceReducer,
+  uvDebugTexture: uvDebugTextureSliceReducer,
 });
 
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
   return configureStore({
     reducer: rootReducer,
     preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          // Ignore these action types
+          ignoredActions: ["fetchUVDebugTexture/fulfilled"],
+          // Ignore these paths in the state
+          ignoredPaths: ["uvDebugTexture.value"],
+        },
+      }),
   });
 };
 
