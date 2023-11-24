aecb472c932f017ecce89037b9f6ff1719cda305
Maxim Kuznetsov <maxim.kyznetsov@gmail.com>
2023-11-24 11:46:32 +0300

    Added draco geometry filter test

--- src/utils/tiles-filtering/filter-tile.spec.ts
+++ src/utils/tiles-filtering/filter-tile.spec.ts
@@ -2,6 +2,11 @@ import { parse } from "@loaders.gl/core";
 import { filterTile } from "./filter-tile";
 import { I3SContentLoader } from "@loaders.gl/i3s";
 import { TURANGA_CONTENT_LOADER_OPTIONS } from "../../test/data/Turanga_Library/i3s-content-loader-options";
+import {
+  ADMIN_BUILDING_CONTENT_LOADER_OPTIONS,
+  CONTENT_FEATURE_IDS,
+  CONTENT_INDICES,
+} from "../../test/data/Admin_Building_v17/i3s-content-loader-options";
 import { fetch } from "whatwg-fetch";
 import { getTile3d } from "../../test/tile-stub";
 
@@ -28,6 +33,7 @@ describe("filterTile", () => {
 
     const filtersByAttribute = { attributeName: "BldgLevel", value: 0 };
 
+    // apply filter
     const result = await filterTile(mockTile, filtersByAttribute);
     expect(result).toEqual({ isFiltered: true, id: "41513" });
     expect(mockTile.content.indices).toEqual(
@@ -41,6 +47,7 @@ describe("filterTile", () => {
     expect(mockTile.content.userData.originalIndices).toEqual(null);
     expect(mockTile.content.userData.customFilters).toEqual(filtersByAttribute);
 
+    // cancel filter
     const result2 = await filterTile(mockTile, null);
     expect(result2).toEqual({ isFiltered: true, id: "41513" });
     expect(mockTile.content.indices).toEqual(null);
@@ -73,5 +80,39 @@ describe("filterTile", () => {
     expect(result2).toEqual({ isFiltered: true, id: "41513" });
     const result3 = await filterTile(mockTile, filtersByAttribute);
     expect(result3).toEqual({ isFiltered: false, id: "41513" });
+  });
+
+  it("Should filter tile with draco geometry", async () => {
+    const mockTile = getTile3d();
+    mockTile.tileset.loadOptions = { i3s: {} };
+    mockTile.content.featureIds = CONTENT_FEATURE_IDS;
+    mockTile.content.indices = CONTENT_INDICES;
+    mockTile.tileset.tileset.fields =
+      ADMIN_BUILDING_CONTENT_LOADER_OPTIONS._tilesetOptions.fields;
+    mockTile.tileset.tileset.attributeStorageInfo =
+      ADMIN_BUILDING_CONTENT_LOADER_OPTIONS._tilesetOptions.attributeStorageInfo;
+    mockTile.header.attributeUrls =
+      ADMIN_BUILDING_CONTENT_LOADER_OPTIONS._tileOptions.attributeUrls;
+
+    const filtersByAttribute = { attributeName: "BldgLevel", value: 2 };
+
+    // apply filter
+    const result = await filterTile(mockTile, filtersByAttribute);
+    expect(result).toEqual({ isFiltered: true, id: "41513" });
+    expect(mockTile.content.indices).toEqual(
+      new Uint32Array([
+        464, 465, 466, 466, 465, 467, 467, 465, 468, 468, 465, 469, 465, 464,
+        469, 469, 464, 470, 464, 466, 470, 466, 467, 470, 470, 467, 471, 467,
+        468, 471, 468, 469, 471, 470, 471, 469,
+      ])
+    );
+    expect(mockTile.content.userData.originalIndices.length).toEqual(2544);
+    expect(mockTile.content.userData.customFilters).toEqual(filtersByAttribute);
+
+    // cancel filter
+    const result2 = await filterTile(mockTile, null);
+    expect(result2).toEqual({ isFiltered: true, id: "41513" });
+    expect(mockTile.content.indices.length).toEqual(2544);
+    expect(mockTile.content.userData.customFilters).toEqual(null);
   });
 });
