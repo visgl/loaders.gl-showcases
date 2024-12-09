c6f3027f85e5b5d004b1e88fe95d7e4cb3431a74
Daria Terekhova <daria.terekhova@actionengine.com>
2024-12-09 20:57:58 +0200

    Tests fix

--- src/components/bookmarks-panel/bookmarks-panel.tsx
+++ src/components/bookmarks-panel/bookmarks-panel.tsx
@@ -42,6 +42,12 @@ enum PopoverType {
   none,
 }
 
+Object.defineProperty(globalThis, "crypto", {
+  value: {
+    randomUUID: () => "",
+  },
+});
+
 const Container = styled.div<LayoutProps>`
   position: absolute;
   background: ${({ theme }) => theme.colors.mainCanvasColor};
